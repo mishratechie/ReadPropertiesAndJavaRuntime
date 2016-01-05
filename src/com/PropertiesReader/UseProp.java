4382a31ae83f0c197d36bcf3f09600933d913cc2
Ashutosh <mishra.techie@gmail.com>
2016-01-05 08:13:37 +0530

    Removed Warnings

--- src/com/PropertiesReader/UseProp.java
+++ src/com/PropertiesReader/UseProp.java
@@ -1,9 +1,6 @@
 package com.PropertiesReader;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Properties;
 
 public class UseProp {
